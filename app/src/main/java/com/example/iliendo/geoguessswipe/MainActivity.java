15eb53674e77fb75a5683db157655ba9e6e2e439
Ilias Azagagh <ilias.azagagh@hva.nl>
2019-04-14 16:22:52 +0200

    initial commit

---
+++ app/src/main/java/com/example/iliendo/geoguessswipe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.iliendo.geoguessswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
